be3cacd8f9b85d169bcdcc6c4d55c95f09cf015a
Sanat Shah <sunnyshah711@gmail.com>
2015-10-04 20:05:43 -0400

    started testing in main.c

--- SortedList/main.c
+++ SortedList/main.c
@@ -5,10 +5,10 @@
 
 
 
-int compFloats(void * p1, void * p2 ){
+int compInt(void * p1, void * p2 ){
 
-        float   * x1 = (float *)p1;
-        float   * x2 = (float *)p2;
+        int   * x1 = (int *)p1;
+        int   * x2 = (int *)p2;
 
         if ( *x1 < *x2 )
                 return -1;
@@ -18,19 +18,30 @@ int compFloats(void * p1, void * p2 ){
                 return 0;
 }
 
-void destoryFunc(void *p){
+void destroyFunc(void *p){
 	free(p);
 }
-
-int (*compFloats)(void *,void *);
-int (*destroyFunc)(void *);
 
 int main (int argc, char ** argv){
 
 	printf("Testing Int Comparing \n");
 
-	SLCreate(compFloats,destroyFunc);
 	
+	SortedListPtr slInt=SLCreate(compInt,destroyFunc);
+	printf("Input is: 3, 5, 1, 8, 9");
 
+	int x = 3; 
+	int y = 5; 
+	int a = 1; 
+	int b = 8; 
+	int c = 6; 	
+
+	SLInsert(slInt, (void *)&x);
+	SLInsert(slInt, (void *)&y);
+	SLInsert(slInt, (void *)&a);
+	SLInsert(slInt, (void *)&b);
+	SLInsert(slInt, (void *)&c);
+
+	
 	return 0;
 }
