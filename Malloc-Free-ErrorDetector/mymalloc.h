754e2ffc11826bc1c36c768de87b4dcc53559a63
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-10-18 14:34:06 -0400

    updates

---
+++ Malloc-Free-ErrorDetector/mymalloc.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MYMALLOC_H
+
+typedef struct memBlock memBlock;
+struct memBlock{ 		// memBlock struct for each memory "node"
+	memBlock *prev;
+	memBlock *next;
+	int isFree;
+	int size;
+};
+
+// Function definitions to be implemented in malloc.c
+
+void *mymalloc(unsigned int size, char * file, int line);
+void *mycalloc(int numEle, unsigned int size, char *file, int line);
+void *myrealloc(void *ptr, unsigned int size, char *file, int line);
+void myfree(void *ptr, char *file, int line);
+
