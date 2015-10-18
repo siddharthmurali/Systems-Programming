c3df811fa584e55bb8e7e7995f0abc452894c2f3
Sanat Shah <sunnyshah711@gmail.com>
2015-10-18 19:27:35 -0400

    intialize in mymalloc.

--- Malloc-Free-ErrorDetector/mymalloc.c
+++ Malloc-Free-ErrorDetector/mymalloc.c
@@ -2,9 +2,50 @@
 
 //Need to add definitions, etc...
 
-
-
+static memBlock* front; 
+static memBlock* end;
 
 void *mycalloc(int numEle, unsigned int size, char *file, int line){
 
+
+
+}	
+
+
+void *mymalloc(unsigned int size, char * file, int line){
+	static int initialized =0;
+
+	
+	if (front==null)  
+		intialize=1;
+	
+	if (!intialize){
+		front = (memBlock*)malloc(sizeof(memBlock)); 
+		for (int x=0; x<5000; x++){
+			front->next = (memBlock*)malloc(sizeof(memBlock)); 
+			font=front->next;	
+		{
+	}
+
+
+	 
+
+}
+
+void *myrealloc(void *ptr, unsigned int size, char *file, int line){
+
+
+
+
+
+} 
+
+
+void myfree(void *ptr, char *file, int line){
+
+
+
+
+
+}
 	
