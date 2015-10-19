652fa166a7e9b7309de6ba263108d9cb964e5693
Sanat Shah <sunnyshah711@gmail.com>
2015-10-19 13:28:52 -0400

    pull

--- Malloc-Free-ErrorDetector/mymalloc.c
+++ Malloc-Free-ErrorDetector/mymalloc.c
@@ -1,9 +1,10 @@
-#include <mymalloc.h>
+#include "mymalloc.h"
 
 //Need to add definitions, etc...
 
 static memBlock* front; 
 static memBlock* end;
+int debug = 1;
 
 void *mycalloc(int numEle, unsigned int size, char *file, int line){
 
@@ -12,44 +13,62 @@ void *mycalloc(int numEle, unsigned int size, char *file, int line){
 }	
 
 
-void *mymalloc(unsigned int size, char * file, int line){
+void *mymalloc(unsigned int size, char * file, int line) {
 	static int initialized = 0;
+	int x=0;
 	
-	if (front==null)  
-		intialize=0;
-	
-	if (!intialize){
+	if (front==NULL) { 
+		initialized=0;
+	}
+
+	if (debug) 
+		printf("Before initialization");
+
+	if (!initialized){
 		front = (memBlock*)malloc(sizeof(memBlock)); 
+		front->isFree = 1;
+		front->size = 0; 
+		front->prev=NULL;
+		front->next=NULL;
 		memBlock* fptr=front;
+		memBlock* bptr=front;
+		front=front->next;
 
-		for (int x=0; x<5000; x++){
+		for (x=0; x<5000; x++){
 
-			front->next = (memBlock*)malloc(sizeof(memBlock)); 
+			front= (memBlock*)malloc(sizeof(memBlock)); 
 			front->isFree = 1;
-			front->size = 0; 
-			font=front->next;	
-		{
+			front->size = 1; 
+			front->prev=bptr;
+			front->next=NULL;
+			bptr=front;
+			front=front->next;	
+		}
 		
 		end = front;
 		front = fptr;
 	}
 	
 	if (size==0){
-		printf("Can't allocate size 0");
+		printf("ERROR: Can't allocate size 0");
 		return (void *) 0;
 	}
 
-	bool spaceCheck = false; 
+	int spaceCheck = 0;
 	int count = 0 ;
 	memBlock* nodeDelptr = end;
 
 	while(nodeDelptr->isFree) {
 		count++;
 		if (count==size) {
-			spaceCheck=true; 
+			spaceCheck=1; 
 			break;	
 
-		}
+		} 
+
+		if (count>size) 
+			break;
+
 		nodeDelptr=nodeDelptr->prev;	
 	}	
 
@@ -59,12 +78,10 @@ void *mymalloc(unsigned int size, char * file, int line){
 		return (void *) 0;
 	}
 
-	while(nodeDelptr->next!=null){
+	while(nodeDelptr->next!=NULL){
 
 		nodeDelptr=nodeDelptr->next; 
 		free(nodeDelptr->prev);	
-
-
 	}
 
 	
@@ -74,9 +91,8 @@ void *mymalloc(unsigned int size, char * file, int line){
 	newBlock->size=size; 
 	
 	
-	return newBlock->data;
+	return (void *)newBlock->data;
 
-	 
 
 }
 
--- Malloc-Free-ErrorDetector/mymalloc.h
+++ Malloc-Free-ErrorDetector/mymalloc.h
@@ -3,14 +3,19 @@
 #include <string.h>
 
 #define MYMALLOC_H
+#define malloc( x ) mymalloc( x, __FILE__, __LINE__ )
+#define free( x ) myfree( x, __FILE__, __LINE__ )
+#define calloc( x , y ) mycalloc( x, y, __FILE__, __LINE__ )
+#define realloc( x , y ) myrealloc( x, y, __FILE__, __LINE__ )
+
 
 typedef struct memBlock memBlock;
 struct memBlock{ 		// memBlock struct for each memory "node"
 	memBlock *prev;
 	memBlock *next;
-	void data; 
 	int isFree;
 	int size;
+	void* data; 
 };
 
 // Function definitions to be implemented in malloc.c
