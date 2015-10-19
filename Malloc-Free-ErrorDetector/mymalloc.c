c45d3159d4b0e2bc967723f49490ab03b6815cfe
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-10-18 20:00:40 -0400

    twitter check

--- Malloc-Free-ErrorDetector/mymalloc.c
+++ Malloc-Free-ErrorDetector/mymalloc.c
@@ -4,7 +4,18 @@
 
 
 
+//Macro Definitions
 
+#define mymalloc( x ) mymalloc(x, __FILE__, __LINE__)
+#define myfree( x) myfree(x, __FILE__, __LINE__ )
+#define mycalloc(x) mycalloc(x, __FILE__, __LINE__)
+#define myrealloc(x) myrealloc(x, __FILE__, __LINE__)
+
+
+
+
+void myfree(void *ptr, char *file, int line){
+	
 void *mycalloc(int numEle, unsigned int size, char *file, int line){
 
 	
