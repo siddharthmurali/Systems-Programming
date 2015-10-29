4f4b9726463261e3291079d103b7807a99a72a44
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-10-28 21:13:52 -0400

    indexer

---
+++ Indexer/indexer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "indexerTokenizer.c"
+
+int main(int argc, char* argv[]){
+	
+}
+
+
---
+++ Indexer/indexerTokenizer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char[]* tokenate(FILE f){
+
+	//Get input file and create list of tokens for each file 
+
+
+}
