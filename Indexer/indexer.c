d8de063d96477853886306ea7015f2eb693c9162
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-10-29 12:53:17 -0400

    Layout stuff

--- Indexer/indexer.c
+++ Indexer/indexer.c
@@ -3,7 +3,17 @@
 #include "indexerTokenizer.c"
 
 int main(int argc, char* argv[]){
+
+	//Get initial input and depending on input type, go through directory or a single file
+
+	//while loop to keep asking for more input
+
 	
 }
+
+//Add helper functions sorting?
+
+
+
 
 
