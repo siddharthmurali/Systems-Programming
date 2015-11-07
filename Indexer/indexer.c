f69132e5a4ac3ce477971fbb311dbc16079885b9
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-11-07 13:46:49 -0500

    Can open dir and see files, but cannot open files

--- Indexer/indexer.c
+++ Indexer/indexer.c
@@ -5,15 +5,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+
+static void cat(FILE *fp)
+{
+    char   buffer[4096];
+    size_t nbytes;
+    while ((nbytes = fread(buffer, sizeof(char), sizeof(buffer), fp)) != 0)
+         fwrite(buffer, sizeof(char), nbytes, stdout);
+}
+
 
 int main(int argc, char* argv[]){
 	
 	int isDirBool = 0;
 	struct stat statbuf;
-
-	FILE *fp = fopen(argv[1], "r");
+	struct dirent *dir;
+	int c;
+	DIR *d;
+	FILE *fp;
+	char str[200];
+	fp = fopen(argv[1], "r");
 		if(fp == NULL){
 			printf("is null\n");
+			fclose(fp);
 		} 
 
 		stat(argv[1], &statbuf);
@@ -21,14 +38,59 @@ int main(int argc, char* argv[]){
 		if(S_ISDIR(statbuf.st_mode)){
 			printf("directory\n");
 			isDirBool = 1;
+			fclose(fp);
 		}
 		else{
 			printf("file\n");
+			while(1){
+				c = fgetc(fp);
+				if(feof(fp)){
+					break;
+				}
+				printf("%c", c);
+			}
+			fclose(fp);
 		}
+
+		FILE *fileIter;
 
 		if(isDirBool == 1){
 			
+			d = opendir(argv[1]);
+			while ((dir = readdir(d))){
 
+     				if (!strcmp (dir->d_name, ".")){
+           				continue;
+				}
+       				if (!strcmp (dir->d_name, "..")){    
+           				continue;
+				}
+				
+				printf("%s\n", dir->d_name);
+				fileIter = fopen(dir->d_name, "rw");
+     				if(fileIter == NULL){
+					printf("file is Null\n");
+					fprintf(stderr, "Error : Failed to open file - %s\n", strerror(errno));
+
+        return 1;
+				}
+				else{
+					while(1){
+						c = fgetc(fileIter);
+						if(feof(fileIter)){
+							break;
+						}
+						printf("%c", c);
+					}
+									
+  					fclose(fileIter);
+  				}
+			 }
+				
+   			 closedir(d);
+		}
+
+	return 0;
 		
 }
 
