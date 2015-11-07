e73918d675f115cd37d0d3b41dad1a619d437583
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-11-07 17:46:40 -0500

    indexerTokenizer.c but not compilable yet

--- Indexer/indexerTokenizer.c
+++ Indexer/indexerTokenizer.c
@@ -1,13 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-char* tokenate(FILE f){
+typedef struct token* tokenPtr;
 
-	//Get input file and create list of tokens for each file 
+struct token{
+	char word[50];
+	char fileName[50];
+	int freq;
+	tokenPtr next;
+};
 
 
+struct tokenStream{
+	tokenPtr head;
+	tokenPtr token;
+	tokenPtr next;
+};
+typedef struct tokenStream* tokenStreamPtr;
 
+tokenPtr tokenate(tokenPtr head, FILE f){
+	
+	FILE *fp;
+	fp = fopen(f, "r");
+	char line[300];
+	char c;
+	int linePlaceHolder = 0;
+	int lineCountCurr = 0;
+	char tmp[50];
+	tokenPtr newToken;
+	tokenPtr tokenIter;
+	while(fgets(line, sizeof(line), fp) != NULL){
+		c = line[lineCountCurr];			 
+		while(1){
+			if(isalnum(c)){
+				c = line[lineCountCurr++];
+			}
+			else{
+				newToken =(tokenPtr) malloc(sizeof( struct token));			
+				linePlaceHolder;
+				while(linePlaceHolder != lineCountCurr){
+					tmp[linePlaceHolder] = line[linePlaceHolder];
+					linePlaceHolder++;
+				}
+				newToken->word = tmp;
+				if(head == NULL){
+					head = newToken;
+				}
+				else{
+					tokenIter =(tokenPtr)malloc(sizeof(struct token));
+					while(tokenIter->next != NULL){
+						tokenIter = tokenIter->next;
+					}
+					tokenIter->next = newToken;
+				} 		
+			}
+							 
+			if(lineCountCurr > 299){
+				break;
+			} 
+		}
 
-
-
+	}
 }
