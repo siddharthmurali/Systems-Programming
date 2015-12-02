ebde5f543906243bbe99faa8b0bb9315dba7e7e0
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-12-02 13:16:03 -0500

    actually now finished

--- BankSystem/bankAccount.c
+++ BankSystem/bankAccount.c
@@ -1,5 +1,4 @@
 #include "bankAccount.h"
-#define MAX_ACCOUNTS 20 // The max number of bank accounts that can be created
 
 //number of accounts initially
 accountCount;
@@ -7,9 +6,13 @@ accountCount;
 //create pointer to list of bank accounts
 bankAccount* list;
 
+
+int i;
+
+
 void initList(){
 
-	list = malloc(MAX_ACCOUNTS * sizeof(bankAccount));
+	list = malloc(20 * sizeof(bankAccount));
 	accountCount = 0;
 
 	return;
@@ -18,20 +21,34 @@ void initList(){
 
 BankAccount initBankAccount(char* accountName){
 
+	if(accountCount + 1 == 20){
+		return -1;
+	}
+	
+	for(i=0; i<accountCount; i++){
+		if(list[i]->name == accountName){
+			return -1;
+		}
+	}
+
+	if(accountName == NULL){
+		return -1;
+	}
+
 	//create mem space for new account
-	BankAccount account = malloc(sizeof(bankAccount));
+	BankAccount account = malloc(sizeof(struct bankAccount));
 
 	//fill in all the fields
-	account->isf = 0;
-	account->balance =0;
-	account->name = accountName;
+	account->isf = 1;
+	account->balance =0.0;
+
+	account->name = malloc(100*sizeof(char));
+	strcpy(account->name, accountName);
 
 	return account;
 }
 
 void printList(){
-
-	int i;
 
 	for(i=0; i<accountCount; i++){
 		printf("%s\n", list[i]->name);
